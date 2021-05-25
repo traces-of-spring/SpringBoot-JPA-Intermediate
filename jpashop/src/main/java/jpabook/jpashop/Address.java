f6687819267a66715bc5e75a5926c00ab9665482
yeGenieee <cyj95428@gmail.com>
2021-05-25 17:30:42 +0900

    fix: 값 타입은 immutable 하게 설계해야 하므로 @Setter 제거

--- jpashop/src/main/java/jpabook/jpashop/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/Address.java
@@ -7,10 +7,19 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
-@Setter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
