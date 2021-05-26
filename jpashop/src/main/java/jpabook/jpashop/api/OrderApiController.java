09e2a5b2a57e8cc0b4458b2e97a1f5cd1066fddd
yeGenieee <cyj95428@gmail.com>
2021-05-27 06:42:31 +0900

    feat: 복잡한 주문 조회 API V3 구현 (엔티티를 DTO로 반환 - 페치 조인 최적화)

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -23,20 +23,35 @@ public class OrderApiController {
 
     private final OrderRepository orderRepository;
 
+    /**
+     * V1. 엔티티 직접 노출
+     * - 엔티티가 변하면 API 스펙이 변한다.
+     * - 트랜잭션 안에서 지연 로딩 필요
+     * - 양방향 연관관계 문제
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     * @return
+     */
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAll(new OrderSearch());
         for (Order order : all) {
-            order.getMember().getName();
-            order.getDelivery().getAddress();
+            order.getMember().getName(); // LAZY 강제 초기화
+            order.getDelivery().getAddress(); // LAZY 강제 초기화
             List<OrderItem> orderItems = order.getOrderItems();
             orderItems.stream()
-                    .forEach(o -> o.getItem().getName());
+                    .forEach(o -> o.getItem().getName()); // LAZY 강제 초기화
         }
 
         return all;
     }
 
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환 (fetch join 사용 X)
+     * - 트랜잭션 안에서 지연 로딩 필요
+     * - 지연 로딩으로 너무 많은 SQL 실행
+     * @return
+     */
     @GetMapping("/api/v2/orders")
     public Result ordersV2() {
         List<Order> all = orderRepository.findAll(new OrderSearch());
@@ -45,6 +60,21 @@ public class OrderApiController {
                 .map(OrderDTO::new)
                 .collect(Collectors.toList());
 
+        return new Result(collect);
+    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환 (fetch join 사용 O)
+     * - 페이징 시에는 N 부분을 포기해야함 (대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+     * - 페치 조인으로 SQL이 1번만 실행됨
+     * @return
+     */
+    @GetMapping("/api/v3/orders")
+    public Result ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDTO> collect = orders.stream()
+                .map(OrderDTO::new)
+                .collect(Collectors.toList());
         return new Result(collect);
     }
 
@@ -66,11 +96,11 @@ public class OrderApiController {
 
         public OrderDTO(Order order) {
             orderId = order.getId();
-            name = order.getMember().getName();
+            name = order.getMember().getName(); // LAZY
             orderDate = order.getOrderDate();
             orderStatus = order.getStatus();
-            address = order.getDelivery().getAddress();
-            orderItems = order.getOrderItems().stream()
+            address = order.getDelivery().getAddress(); // LAZY
+            orderItems = order.getOrderItems().stream() // LAZY
                     .map(OrderItemDTO::new)
                     .collect(Collectors.toList());
         }
@@ -85,7 +115,7 @@ public class OrderApiController {
         private int count;
 
         public OrderItemDTO(OrderItem orderItem) {
-            itemName = orderItem.getItem().getName();
+            itemName = orderItem.getItem().getName(); // LAZY
             orderPrice = orderItem.getOrderPrice();
             count = orderItem.getCount();
         }
